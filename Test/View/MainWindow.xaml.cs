731cae5cdc2a2594da15df60e16d75ab07258645
Alexey <Login-alex200@yandex.ru>
2023-12-02 18:40:46 +0300

    Выделение пространства имен Test.View

---
+++ Test/View/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.View;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow() => InitializeComponent();
+}

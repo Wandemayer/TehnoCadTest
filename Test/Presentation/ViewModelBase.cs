a5210a081d789b579ad4b70955409b9da92cc405
Alexey <Login-alex200@yandex.ru>
2023-12-03 15:11:07 +0300

    Добавление ViewModelBase

---
+++ Test/Presentation/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using Test.Common;
+
+namespace Test.Presentation;
+
+/// <summary>
+/// Базовый абстрактный класс модели представления.
+/// </summary>
+public abstract class ViewModelBase : ObservedElement
+{
+    
+}

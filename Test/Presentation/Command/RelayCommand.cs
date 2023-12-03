23b5243990b0a39a46180b5be0d687b95d219375
Alexey <Login-alex200@yandex.ru>
2023-12-03 15:10:51 +0300

    Добавление реализаций ICommand

---
+++ Test/Presentation/Command/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Test.Presentation.Command;
+
+/// <summary>
+/// Определяет команду для выполнения.
+/// </summary>
+public class RelayCommand : ICommand
+{
+    private readonly Action _execute;
+    private readonly Func<bool>? _canExecute;
+
+    public RelayCommand(Action execute, Func<bool>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    /// <inheritdoc />
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    /// <inheritdoc />
+    public void Execute(object? parameter)
+    {
+        if (CanExecute(parameter))
+        {
+            _execute();
+        }
+    }
+
+    /// <inheritdoc />
+    public bool CanExecute(object? parameter)
+    {
+        if (_canExecute is null)
+        {
+            return true;
+        }
+
+        return _canExecute();
+    }
+}
---
+++ Test/Presentation/Command/RelayCommand`1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace Test.Presentation.Command;
+
+#nullable disable
+
+/// <summary>
+/// Определяет команду для выполнения с возможностью передачи параметра заданного типа.
+/// </summary>
+/// <typeparam name="T">Тип параметра команды.</typeparam>
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T> _execute;
+    private readonly Predicate<T> _canExecute;
+    private readonly bool _paramIsValue;
+
+    public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+        _paramIsValue = typeof(T).GetTypeInfo().IsValueType;
+    }
+
+    /// <inheritdoc />
+    public event EventHandler CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    /// <inheritdoc />
+    public void Execute(object parameter)
+    {
+        if (CanExecute(parameter))
+        {
+            _execute((T)parameter);
+        }
+    }
+
+    /// <inheritdoc />
+    public bool CanExecute(object parameter)
+    {
+        if (_canExecute is null)
+        {
+            return true;
+        }
+
+        return parameter switch
+        {
+            null when _paramIsValue => _canExecute.Invoke(default),
+            null or T => _canExecute.Invoke((T)parameter),
+            _ => false
+        };
+    }
+}
